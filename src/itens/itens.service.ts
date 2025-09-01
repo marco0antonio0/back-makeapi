1501d918cd541ae3850449731d1889ef1365dc5a
falcaoNeto <falcao.coke@gmail.com>
2025-09-01 16:46:27 -0300

    feat/makeapi-29: criacao service

--- src/itens/itens.service.ts
+++ src/itens/itens.service.ts
@@ -3,6 +3,7 @@ import { ItensRepository } from './itens.repository'
 import { CreateItemDto } from './dtos/create-item.dto'
 import { UpdateItemDto } from './dtos/update-item.dto'
 import { ListItemsQueryDto } from './dtos/list-query.dto'
+import { ListItemsPaginationQueryDto } from './dtos/list-query-pagination.dto'
 import { QueryItemDto } from './dtos/query-item.dto'
 import { ItemEntity } from './entities/item.entity'
 
@@ -15,6 +16,23 @@ export class ItensService {
   }
   findAll(query: ListItemsQueryDto): Promise<ItemEntity[]> {
     return this.repo.findAll({ endpointId: query.endpointId, limit: query.limit })
+  }
+
+  async findAllWithPagination(query: ListItemsPaginationQueryDto): Promise<ItemEntity[]> {
+    let lastId : string
+    let itens : ItemEntity[]
+      for (let i = 0; i < query.page; i++) {
+
+          if (i === 0) {
+            console.log('primeira vez')
+            itens = await this.repo.findAllWithPagination({ endpointId: query.endpointId, limit: query.limit})
+            lastId = itens[itens.length - 1].id
+            continue
+          }
+        itens = await this.repo.findAllWithPagination({ endpointId: query.endpointId, limit: query.limit, startAfterId: lastId})
+        lastId = itens[itens.length - 1].id
+      }
+      return itens
   }
   findOne(id: string): Promise<ItemEntity> {
     return this.repo.findById(id)
